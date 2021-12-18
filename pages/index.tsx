cc7513763ce5c8167e727a11bcecebbd4967ff0f
Yuya Tanaka <yuya.presto@gmail.com>
2021-12-18 18:17:04 +0900

    Extract createMarkers

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,10 +4,8 @@ import { AppBar, Button, CircularProgress, Divider, Paper, Stack, Toolbar, Typog
 import { Box } from '@mui/system'
 import { GetStaticProps } from 'next'
 import Head from 'next/head'
-import { useEffect, useRef } from 'react'
-import { AdmissionDifficulty, getAdmissionDifficulty } from '../lib/model/nursery-school'
 import { getAllNurserySchoolListSets, LocalNurserySchoolListSet } from '../lib/model/nursery-school-list'
-import { blue } from '../styles/theme'
+import { createMarkers } from '../components/marker'
 
 interface Props {
   nurserySets: LocalNurserySchoolListSet[]
@@ -107,38 +105,6 @@ function MyMapComponent({
 
   return <Box ref={ref} id="map" sx={{ width: '100%', height: '100%' }} />
 }
-
-const markerPath =
-  'M20.8737 31.495C27.2804 29.7873 32 23.9448 32 17C32 8.71573 25.2843 2 17 2C8.71573 2 2 8.71573 2 17C2 23.9448 6.71957 29.7873 13.1263 31.495L17 41L20.8737 31.495Z'
-
-function createMarkers(map: google.maps.Map, nurserySets: LocalNurserySchoolListSet[]) {
-  const markers: google.maps.Marker[] = []
-  for (const nurserySet of nurserySets) {
-    for (const nursery of nurserySet.nurseryShoolList) {
-      const markerStyle = difficultyToStyle[getAdmissionDifficulty(nursery, nurserySet, null)]
-      const marker = new google.maps.Marker({
-        map,
-        title: nursery.name,
-        position: { lat: nursery.location.latitude, lng: nursery.location.longitude },
-        icon: {
-          fillColor: markerStyle.color,
-          fillOpacity: 1,
-          strokeColor: blue[90],
-          strokeWeight: 1.5,
-          path: markerPath,
-        },
-      })
-      markers.push(marker)
-    }
-  }
-  return markers
-}
-
-const difficultyToStyle = Object.freeze<Record<AdmissionDifficulty, { color: string; label: string }>>({
-  [AdmissionDifficulty.Easy]: { color: blue[50], label: '入りやすい' },
-  [AdmissionDifficulty.Moderate]: { color: blue[20], label: 'やや入りやすい' },
-  [AdmissionDifficulty.Hard]: { color: blue[10], label: '入りにくい' },
-})
 
 export const getStaticProps: GetStaticProps<Props> = () => {
   return {
