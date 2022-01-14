1c6ff0832f74284920ac87c70abc17ad7a131f8d
Yuya Tanaka <yuya.presto@gmail.com>
2022-01-15 07:33:23 +0900

    Override react-spring config to change BottomSheet speed

--- next.config.js
+++ next.config.js
@@ -1,3 +1,18 @@
+const { merge } = require('webpack-merge')
+const path = require('path')
+
 module.exports = {
   reactStrictMode: true,
+
+  webpack: (config, { buildId, dev, isServer, defaultLoaders, webpack }) => {
+    // Important: return the modified config
+    return merge(config, {
+      resolve: {
+        alias: {
+          'react-spring': path.resolve(__dirname, 'utils/overrides/react-spring.js'),
+          'orig-react-spring': path.resolve(__dirname, 'node_modules/react-spring')
+        }
+      }
+    })
+  },
 }
---
+++ utils/overrides/react-spring.js
@@ -0,0 +1,6 @@
+const orig = require('orig-react-spring')
+
+orig.config.default.tension = 320
+orig.config.default.friction = 32
+
+module.exports = orig
