5d87e7eddc5b5a6504803837b43d9ec1329268c8
sagaraya <sagaraya@gmail.com>
2022-03-20 15:28:46 +0900

    Update translated json

--- lib/data/minato-ku/translator.ts
+++ lib/data/minato-ku/translator.ts
@@ -1,127 +1,123 @@
 import { csv2json } from '../csv2json'
-import {
-  validateClass,
-  validateNumber,
-  validateString,
-  validateOptionalString,
-  validateSource
-} from '../validator'
-import {
-  ClassList,
-  createClass,
-  GeoLocation,
-  NurserySchool,
-  Source
- } from '../../model/nursery-school'
+import { validateClass, validateNumber, validateString, validateOptionalString, validateSource } from '../validator'
+import { ClassList, createClass, GeoLocation, NurserySchool, Source } from '../../model/nursery-school'
 import { MergedData } from '../../model/data'
 
+// 保育園のWebサイトのURLのみ、手動で入力したものを利用
+// 将来的には、オープンデータのなかに追加される予定
 const minatoKuHoikuen: Source = {
-  name: "港区内認可保育園等一覧",
-  ver: "Ver202104",
-  url: "https://catalog.data.metro.tokyo.lg.jp/dataset/t131032d0000000001/resource/a208f801-ce87-4fb9-96d0-ba38813ea330",
-  filePath: __dirname + "/hoikuen.csv"
+  name: '港区内認可保育園等一覧',
+  ver: 'Ver202104',
+  url: 'https://catalog.data.metro.tokyo.lg.jp/dataset/t131032d0000000001/resource/a208f801-ce87-4fb9-96d0-ba38813ea330',
+  filePath: __dirname + '/hoikuen.csv',
 }
 
 const minatoKuHoikuenSaiteiShisu: Source = {
-  name: "令和3年4月入所1次利用調整における入所者最低指数",
-  ver: "Ver202104",
-  url: "https://catalog.data.metro.tokyo.lg.jp/dataset/t131032d0000000001/resource/e81871d0-1d09-453b-b492-2321e3335e26",
-  filePath: __dirname + "/hoikuen_saiteishisu.csv"
+  name: '令和3年4月入所1次利用調整における入所者最低指数',
+  ver: 'Ver202104',
+  url: 'https://catalog.data.metro.tokyo.lg.jp/dataset/t131032d0000000001/resource/e81871d0-1d09-453b-b492-2321e3335e26',
+  filePath: __dirname + '/hoikuen_saiteishisu.csv',
 }
 
 const minatoKu: MergedData = {
-  localName: "港区",
+  localName: '港区',
   dataList: [
     {
       filePath: minatoKuHoikuen.filePath,
       mapping: [
         {
-          tsModelKey: "institutionType",
+          tsModelKey: 'institutionType',
           valueTranslator: (record: any): string => {
-            return validateString(record["施設種別"])
-          }
+            return validateString(record['施設種別'])
+          },
         },
         {
-          tsModelKey: "area",
+          tsModelKey: 'area',
           valueTranslator: (record: any): string => {
-            return validateString(record["地区"])
-          }
+            return validateString(record['地区'])
+          },
         },
         {
-          tsModelKey: "name",
+          tsModelKey: 'name',
           valueTranslator: (record: any): string => {
-            return validateString(record["保育園名"])
-          }
+            return validateString(record['保育園名'])
+          },
         },
         {
-          tsModelKey: "address",
+          tsModelKey: 'address',
           valueTranslator: (record: any): string => {
-            return validateString(record["所在地"])
-          }
+            return validateString(record['所在地'])
+          },
         },
         {
-          tsModelKey: "location",
+          tsModelKey: 'location',
           valueTranslator: (record: any): GeoLocation => {
             const value: GeoLocation = {
-              latitude: validateNumber(parseFloat(record["緯度"])),
-              longitude: validateNumber(parseFloat(record["経度"]))
+              latitude: validateNumber(parseFloat(record['緯度'])),
+              longitude: validateNumber(parseFloat(record['経度'])),
             }
             return value
-          }
+          },
         },
         {
-          tsModelKey: "ownerType",
+          tsModelKey: 'ownerType',
           valueTranslator: (record: any): string => {
-            return validateString(record["種別"])
-          }
+            return validateString(record['種別'])
+          },
         },
         {
-          tsModelKey: "tel",
+          tsModelKey: 'tel',
           valueTranslator: (record: any): string => {
-            return validateString(record["電話番号"])
-          }
+            return validateString(record['電話番号'])
+          },
         },
         {
-          tsModelKey: "admissionAgeInMonth",
+          tsModelKey: 'url',
           valueTranslator: (record: any): string | null => {
-            return validateOptionalString(record["入園可能月齢"])
-          }
+            return validateOptionalString(record['WebサイトURL'])
+          },
         },
         {
-          tsModelKey: "sources",
+          tsModelKey: 'admissionAgeInMonth',
+          valueTranslator: (record: any): string | null => {
+            return validateOptionalString(record['入園可能月齢'])
+          },
+        },
+        {
+          tsModelKey: 'sources',
           valueTranslator: (record: any): Source[] => {
             return [validateSource(minatoKuHoikuen)]
-          }
+          },
         },
-      ]
+      ],
     },
     {
       filePath: minatoKuHoikuenSaiteiShisu.filePath,
       mapping: [
         {
-          tsModelKey: "name",
+          tsModelKey: 'name',
           valueTranslator: (record: any): string => {
-            return validateString(record["保育園名"])
-          }
+            return validateString(record['保育園名'])
+          },
         },
         {
-          tsModelKey: "classList",
+          tsModelKey: 'classList',
           valueTranslator: (record: any): ClassList => {
             const value: ClassList = {
-              age0: validateClass(createClass(record["0歳"])),
-              age1: validateClass(createClass(record["1歳"])),
-              age2: validateClass(createClass(record["2歳"])),
-              age3: validateClass(createClass(record["3歳"])),
-              age4: validateClass(createClass(record["4歳"])),
-              age5: validateClass(createClass(record["5歳"])),
-              minimumIndexSource: validateSource(minatoKuHoikuenSaiteiShisu)
+              age0: validateClass(createClass(record['0歳'])),
+              age1: validateClass(createClass(record['1歳'])),
+              age2: validateClass(createClass(record['2歳'])),
+              age3: validateClass(createClass(record['3歳'])),
+              age4: validateClass(createClass(record['4歳'])),
+              age5: validateClass(createClass(record['5歳'])),
+              minimumIndexSource: validateSource(minatoKuHoikuenSaiteiShisu),
             }
             return value
-          }
+          },
         },
-      ]
+      ],
     },
-  ]
+  ],
 }
 
 csv2json(minatoKu)
