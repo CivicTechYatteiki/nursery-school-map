db0f0fc191c76f744e9d8f39cd24f37222e88f2f
Yuya Tanaka <yuya.presto@gmail.com>
2022-03-21 03:33:59 +0900

    Implement move to current location button

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,24 @@
 import { Status, Wrapper } from '@googlemaps/react-wrapper'
 import ErrorIcon from '@mui/icons-material/Error'
-import { AppBar, Button, CircularProgress, Divider, Paper, Stack, Toolbar, Typography, useTheme } from '@mui/material'
+import {
+  Alert,
+  AlertProps,
+  AppBar,
+  Button,
+  CircularProgress,
+  Divider,
+  Paper,
+  Snackbar,
+  SnackbarProps,
+  Stack,
+  Toolbar,
+  Typography,
+  useTheme,
+} from '@mui/material'
 import { Box, getContrastRatio } from '@mui/system'
 import { GetStaticProps } from 'next'
 import Head from 'next/head'
-import { useRef, useState } from 'react'
+import React, { useRef, useState } from 'react'
 import { use100vh } from 'react-div-100vh'
 import { BottomSheet } from 'react-spring-bottom-sheet'
 import { SpringEvent } from 'react-spring-bottom-sheet/dist/types'
@@ -14,6 +28,9 @@ import { NurseryDetail } from '../components/NurseryDetail'
 import { NurserySchool } from '../lib/model/nursery-school'
 import { getAllNurserySchoolListSets, LocalNurserySchoolListSet } from '../lib/model/nursery-school-list'
 import { useIsomorphicLayoutEffect } from '../utils/useIsomorhpicLayoutEffect'
+
+const DEFAULT_CENTER = { lat: 35.654291, lng: 139.750533 }
+const DEFAULT_ZOOM = 15
 
 interface Props {
   nurserySets: LocalNurserySchoolListSet[]
@@ -32,6 +49,9 @@ export default function Home({ nurserySets }: Props) {
     marker: google.maps.Marker
   } | null>(null)
   const [filter, setFilter] = useState<FilterProps>({ ageList: null })
+  const [snackbarState, setSnackbarState] = useState<
+    (Pick<SnackbarProps, 'open' | 'message'> & Pick<AlertProps, 'severity'>) | null
+  >(null)
 
   const [bottomSheetState, setBottomSheetState] = useState<{
     current?: BottomSheetKind
@@ -49,6 +69,8 @@ export default function Home({ nurserySets }: Props) {
 
   const pageHeight = use100vh()
 
+  const mapRef = useRef<google.maps.Map>(null)
+
   const render = (status: Status) => {
     switch (status) {
       case Status.LOADING:
@@ -63,10 +85,11 @@ export default function Home({ nurserySets }: Props) {
       case Status.SUCCESS:
         return (
           <MyMapComponent
-            center={{ lat: 35.654291, lng: 139.750533 }}
-            zoom={15}
+            center={DEFAULT_CENTER}
+            zoom={DEFAULT_ZOOM}
             nurserySets={nurserySets}
             filter={filter}
+            mapRef={mapRef}
             onClickMarker={({ nursery, inNurserySet, marker }) => {
               resetMarker()
 
@@ -111,6 +134,32 @@ export default function Home({ nurserySets }: Props) {
     detail.marker.setLabel(null)
   }
 
+  const [locationActive, setLocationActive] = useState(false)
+
+  const handleLocation = () => {
+    if (locationActive) return
+    setLocationActive(true)
+
+    navigator.geolocation.getCurrentPosition(
+      (position: GeolocationPosition) => {
+        const pos = {
+          lat: position.coords.latitude,
+          lng: position.coords.longitude,
+        }
+
+        mapRef.current?.setCenter(pos)
+        mapRef.current?.setZoom(DEFAULT_ZOOM)
+        setLocationActive(false)
+      },
+      () => {
+        setSnackbarState({ open: true, severity: 'warning', message: '位置情報の取得に失敗しました' })
+        setLocationActive(false)
+      }
+    )
+  }
+
+  const handleSnackbarClose = () => setSnackbarState(s => ({ ...s, open: false }))
+
   return (
     <>
       <Head>
@@ -133,10 +182,12 @@ export default function Home({ nurserySets }: Props) {
 
         <FilterButton
           filter={filter}
+          locationActive={locationActive}
           onClickFilter={() => {
             setFilter({ ageList: filter.ageList })
             openBottomSheet('filter')
           }}
+          onClickLocation={handleLocation}
         />
 
         <div
@@ -176,11 +227,29 @@ export default function Home({ nurserySets }: Props) {
           />
         )}
       </BottomSheet>
+
+      {snackbarState && (
+        <Snackbar open={snackbarState.open} onClose={handleSnackbarClose} message={snackbarState.message}>
+          <Alert variant="filled" severity={snackbarState.severity} onClose={handleSnackbarClose}>
+            {snackbarState.message}
+          </Alert>
+        </Snackbar>
+      )}
     </>
   )
 }
 
-function FilterButton({ filter, onClickFilter }: { filter: FilterProps; onClickFilter: () => void }) {
+function FilterButton({
+  filter,
+  locationActive,
+  onClickFilter,
+  onClickLocation,
+}: {
+  filter: FilterProps
+  locationActive: boolean
+  onClickFilter: () => void
+  onClickLocation: () => void
+}) {
   const theme = useTheme()
 
   const buttonText = (filter: FilterProps): string => {
@@ -219,6 +288,12 @@ function FilterButton({ filter, onClickFilter }: { filter: FilterProps; onClickF
         <Button sx={{ color: theme.palette.primary.contrastText }} onClick={onClickFilter}>
           {buttonText(filter)}
         </Button>
+        <Button
+          startIcon={locationActive ? <CircularProgress size={16} sx={{ marginBottom: '2px' }} /> : undefined}
+          onClick={onClickLocation}
+        >
+          現在地へ移動
+        </Button>
       </Stack>
     </Paper>
   )
@@ -229,12 +304,14 @@ function MyMapComponent({
   zoom,
   nurserySets,
   filter,
+  mapRef: mapRefProp,
   onClickMarker,
 }: {
   center: google.maps.LatLngLiteral
   zoom: number
   nurserySets: LocalNurserySchoolListSet[]
   filter: FilterProps
+  mapRef?: React.RefObject<google.maps.Map>
   onClickMarker: MarkerClickHandler
 }) {
   const ref = useRef<HTMLDivElement>(null)
@@ -243,16 +320,29 @@ function MyMapComponent({
   const clickHandlerRef = useRef(onClickMarker)
 
   useIsomorphicLayoutEffect(() => {
-    if (!ref.current) return
     if (mapRef.current) return
 
-    mapRef.current = new google.maps.Map(ref.current, {
-      center: { lat: center.lat, lng: center.lng },
-      zoom,
+    mapRef.current = new google.maps.Map(ref.current!, {
       gestureHandling: 'greedy',
       disableDefaultUI: true,
       zoomControl: true,
     })
+    // there is no way to dispose it
+    // https://stackoverflow.com/questions/10485582/what-is-the-proper-way-to-destroy-a-map-instance
+  }, [])
+
+  useIsomorphicLayoutEffect(() => {
+    if (!mapRefProp) return
+    const mMapRefProp = mapRefProp as React.MutableRefObject<google.maps.Map | null>
+    mMapRefProp.current = mapRef.current!
+    return () => {
+      mMapRefProp.current = null
+    }
+  }, [mapRefProp])
+
+  useIsomorphicLayoutEffect(() => {
+    mapRef.current!.setCenter({ lat: center.lat, lng: center.lng })
+    mapRef.current!.setZoom(zoom)
   }, [center.lat, center.lng, zoom])
 
   useIsomorphicLayoutEffect(() => {
